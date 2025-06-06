0b3ba92a060db3444afd15964a3cf56425b57eb4
Avinash Choran <avinashmanojanchoran@gmail.com>
2025-06-06 21:31:48 +0530

    Fixed Header align issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header(props) {
     return (
         <header>
 
-            
+            <div className="ApiSideBar">
             <ApiSideBar setApiKey={props.setApiKey}/>
-
+            </div>
 
 
             <div className="header-center">
